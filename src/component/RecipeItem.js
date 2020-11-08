473c9b4c7de24aad049091a52737f91c82e56495
Nikhil Arora <nikhil.arora@team.telstra.com>
2020-11-08 06:13:22 +0530

    added star button for favorite recipe list

--- src/component/RecipeItem.js
+++ src/component/RecipeItem.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-export default class RecipeItem extends Component {
-
+import {favoriteRecipe} from '../actions';
+import {connect} from 'react-redux';
+class RecipeItem extends Component {
+    constructor(){
+        super();
+        this.state={
+            favorited:false
+        }
+    }
+    favorite(recipe){
+        this.props.favoriteRecipe(recipe);
+        this.setState({favorited:true});
+    }
     render() {
         let { recipe } = this.props;
         console.log("innside recipe item", this.props);
 
         return (
             <div className="recipe-item">
+                {
+                    this.state.favorited ?
+                    <div className="star">
+                        &#9733;
+                    </div> 
+                : 
+                <div 
+                    className="star"
+                    onClick={()=> this.favorite(recipe)}>
+                    &#9734;
+                </div>
+                }
+                
                 <div className="recipe-text">
                     <a href={recipe.href}>
                         <h4>
@@ -25,3 +49,4 @@ export default class RecipeItem extends Component {
         )
     }
 }
+export default connect(null,{favoriteRecipe})(RecipeItem);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import {combineReducers} from 'redux';
-import {SET_RECIPES} from '../actions';
+import {SET_RECIPES,FAVORITE_RECIPE} from '../actions';
+
 function recipes (state=[],action){
     switch(action.type){
         case SET_RECIPES:
@@ -8,6 +9,16 @@ function recipes (state=[],action){
             return state;
     }
 }
+function favoriteRecipes(state=[],action){
+    switch(action.type){
+        case FAVORITE_RECIPE:
+            state=[...state,action.recipe]
+            return state;
+        default:
+            return state;
 
-const rootReducer =combineReducers({recipes});
+    }
+}
+
+const rootReducer =combineReducers({recipes,favoriteRecipes});
 export default rootReducer;
